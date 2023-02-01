a2be828c4fc5d919522717aa3f7d7f228d18ae8c
DovydasMM <dovistk@gmail.com>
2023-02-01 14:49:14 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
   title = 'weather-app';
+
+  API_KEY = `	SQbZMAWU8gzWWL5CCmAMGQ81BgxAQM74`;
+
+  ngOnInit(): void {
+    this.http
+      .get(
+        `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=%09SQbZMAWU8gzWWL5CCmAMGQ81BgxAQM74`
+      )
+      .subscribe((resData) => console.log(resData));
+  }
 }
